8230b8d4beed94d0f0fa85347ef90e82e2661e75
Odin Kroeger <odkr@users.noreply.github.com>
2019-01-05 18:44:21 +0100

    Prepared release.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ REQPYVERSION = (3,)
 # All other metadata.
 METADATA = {
     "name":             NAME,
-    "version":          '0.9.4rc1',
+    "version":          '0.9.4rc2',
     "description":      """Retrieve all classes derived from a class and
                         sort them by a given priority and order, making it
                         easy to draw up and use plugin-like classes.""",
