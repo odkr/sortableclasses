2fefee21292b59af69930f3bb70fb8fa3c5f8a73
Odin Kroeger <odkr@users.noreply.github.com>
2019-01-05 21:57:56 +0100

    Updated release.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def readme(readme_fname: str = "README.rst") -> str:
 NAME = 'sortableclasses'
 
 # Version of this package.
-VERSION = '0.9.4rc19'
+VERSION = '0.9.4rc20'
 
 # All other metadata.
 METADATA = {
